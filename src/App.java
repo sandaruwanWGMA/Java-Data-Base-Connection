7bbc5a5feb49d0777e78cafbd670ce0ce952e82c
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-10-23 12:42:11 +0530

    commit

--- src/App.java
+++ src/App.java
@@ -9,22 +9,22 @@ public class App {
     public static void JDBCexample(String userid, String passwd) {
         try (
                 Connection conn = DriverManager.getConnection(
-                        "jdbc:mysql://localhost:8000/JDBC",
+                        "jdbc:mysql://localhost:3306/JDBC",
                         userid, passwd);
                 Statement stmt = conn.createStatement();) {
             try {
                 stmt.executeUpdate(
-                        "insert into instructor values(’77987’,’Kim’,’Physics’,98000)");
+                        "insert into instructor values(175,'Molindu', 'Physics',98500)");
             } catch (SQLException sqle) {
                 System.out.println("Could not insert tuple. " + sqle);
             }
             ResultSet rset = stmt.executeQuery(
-                    "select dept name, avg (salary) " +
-                            " from instructor " +
-                            " group by dept name");
+                    "SELECT dept_id, COUNT(name) AS instructor_count " +
+                            "FROM instructor " +
+                            " group by dept_id");
             while (rset.next()) {
-                System.out.println(rset.getString("dept name") + " " +
-                        rset.getFloat(2));
+                System.out.println(rset.getString("dept_id") + " " + rset.getInt(2));
+                System.out.println(rset.getInt(0));
             }
         } catch (Exception sqle) {
             System.out.println("Exception : " + sqle);
---
+++ src/PreparedStatements.java
@@ -0,0 +1,33 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PreparedStatements {
+  public static void JDBCexample(String userid, String passwd) {
+    try (
+        Connection conn = DriverManager.getConnection(
+            "jdbc:mysql://localhost:3306/JDBC",
+            userid, passwd);) {
+      try (PreparedStatement pstmt = conn.prepareStatement("SELECT id FROM instructor WHERE id=?");) {
+        pstmt.setInt(1, 3);
+        pstmt.setInt(1, 2);
+
+        ResultSet rs = pstmt.executeQuery();
+        while (rs.next()) {
+          System.out.println(rs.getInt(1));
+        }
+      } catch (SQLException se) {
+        System.out.println(se);
+      }
+    } catch (Exception sqle) {
+      System.out.println("Exception : " + sqle);
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    System.out.println("Hello, World!");
+    JDBCexample("root", "molindu123");
+  }
+}
